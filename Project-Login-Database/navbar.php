35f7210bdb94fecd28c9aa835b5b7a6caf6d752d
Abdullah A. Alam <abdullah.alam.omi@gmail.com>
2016-09-18 02:09:35 -0700

    Added Pagination

--- Project-Login-Database/navbar.php
+++ Project-Login-Database/navbar.php
@@ -1,3 +1,7 @@
+
+
 <?php
-    echo "<a href='output.php'>See Database Records</a> | <a href='password.php'>Change Passwrod</a>";
+
+echo "<a href='output.php'>See Database Records</a> | <a href='password.php'>Change your Password</a>";	
+
 ?>
